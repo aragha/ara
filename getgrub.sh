bc67ede784293117abc9181b58bb1755f84c9813
aragha <67502036+aragha@users.noreply.github.com>
2021-04-30 10:55:23 -0400

    Update getgrub.sh

--- getgrub.sh
+++ getgrub.sh
@@ -1,6 +1,7 @@
 mkdir grubfolder
 cd grubfolder
-wget https://ftp.gnu.org/gnu/grub/grub-2.04.tar.gz
-tar -xvf grub-2.04.tar.gz
+wget https://www.gnu.org/software/grub/manual/grub/grub.txt
+#wget https://ftp.gnu.org/gnu/grub/grub-2.04.tar.gz
+#tar -xvf grub-2.04.tar.gz
 ls
 pwd
