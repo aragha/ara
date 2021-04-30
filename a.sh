208979363267940245db528794c7db51aa5ebef1
aragha <67502036+aragha@users.noreply.github.com>
2021-04-30 16:43:25 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -169,3 +169,4 @@ sudo dnf --releasever=33 --installroot=/mnt --assumeyes install kernel
 sudo dnf --releasever=33 --installroot=/mnt --assumeyes install grub2 efibootmgr shim grub2-efi grub2-efi-modules grub2-tools-extra grub2-tools-efi grub2-pc-modules grub2-pc
 sudo dnf reinstall grub2-efi grub2-pc grub2-pc-modules grub2-tools-efi grub2-tools-extra shim
 efibootmgr --create --disk /dev/sda  --loader /EFI/fedora/grubx64.efi --label "Fedora Grub"
+sudo grub2-install --efi-directory=/mnt/boot/efi --target=x86_64-efi /dev/sda1
