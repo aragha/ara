2e7c5a7ff8380793990c178f35defc7a29248bcb
Live System User <aras2k@yahoo.com>
2021-04-29 20:10:34 -0400

    Get core and other important components on to the mounted disk

---
+++ b.sh
@@ -0,0 +1,3 @@
+sudo dnf --releasever=33 --installroot=/mnt --assumeyes install kernel
+sudo dnf --releasever=33 --installroot=/mnt --assumeyes install dnf sudo
+sudo dnf --releasever=33 --installroot=/mnt --assumeyes groupinstall core 
