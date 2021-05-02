d551218a2c89d90168266febe2185241e3e1ce1e
feduser <aras2k@yahoo.com>
2021-05-02 09:40:52 -0400

    Removing b.sh since it's a replica of install commands in a.sh

--- b.sh
+++
@@ -1,3 +0,0 @@
-sudo dnf --releasever=33 --installroot=/mnt --assumeyes install kernel
-sudo dnf --releasever=33 --installroot=/mnt --assumeyes install dnf sudo
-sudo dnf --releasever=33 --installroot=/mnt --assumeyes groupinstall core 
