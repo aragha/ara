77332711d5be8d201dbd6c3d56213155cb50b1cb
feduser <aras2k@yahoo.com>
2021-05-17 08:48:52 -0400

    added --assumeyes for a few commands

--- updateit.sh
+++ updateit.sh
@@ -1,9 +1,9 @@
-sudo dnf update
-sudo dnf upgrade --refresh
-sudo dnf check
-sudo dnf autoremove
-sudo fwupdmgr get-devices
-sudo fwupdmgr refresh --force
-sudo fwupdmgr get-updates
-sudo fwupdmgr update
-sudo reboot now
+sudo dnf update --assumeyes
+sudo dnf upgrade --refresh --assumeyes
+sudo dnf check --assumeyes
+sudo dnf autoremove --assumeyes
+sudo fwupdmgr get-devices --assumeyes
+sudo fwupdmgr refresh --force --assumeyes
+sudo fwupdmgr get-updates --assumeyes
+sudo fwupdmgr update --assumeyes
+sudo reboot now 
